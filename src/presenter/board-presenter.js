2c31322bbdcfc45862bb7eaff57222f7440cea92
Александр Голубенко <webgolub@gmail.com>
2022-12-20 17:36:24 +0300

    Добавит отображение «заглушки» при отсутствии точек маршрута

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,11 +3,23 @@ import { getOffersByPointType } from '../utils.js';
 import SortView from '../view/sort-view.js';
 // import PointAddView from '../view/point-add-view.js';
 import TripEventsListView from '../view/trip-events-list-view.js';
+import ListEmptyView from '../view/list-empty-view.js';
 import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 
 export default class BoardPresenter {
+<<<<<<< Updated upstream
   tripEventsList = new TripEventsListView();
+=======
+  #destinationsModel = null;
+  #boardContainer = null;
+  #pointsModel = null;
+  #offersModel = null;
+  #points = [];
+  #destinations = [];
+  #offers = [];
+  #tripEventsList = new TripEventsListView();
+>>>>>>> Stashed changes
 
   constructor({
     boardContainer,
@@ -26,7 +38,17 @@ export default class BoardPresenter {
     this.destinations = [...this.destinationsModel.getDestinations()];
     this.offers = [...this.offersModel.getOffers()];
 
+<<<<<<< Updated upstream
     const getDestination = (id) => this.destinations.find((item) => item.id === id );
+=======
+    if (this.#points.length < 1) {
+      render(new ListEmptyView(), this.#boardContainer);
+      return;
+    }
+
+    render(new SortView(), this.#boardContainer.firstElementChild, RenderPosition.AFTEREND);
+    render(this.#tripEventsList, this.#boardContainer);
+>>>>>>> Stashed changes
 
 
     render(new SortView(), this.boardContainer.firstElementChild, RenderPosition.AFTEREND);
