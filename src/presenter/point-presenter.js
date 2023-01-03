6b1ccf39a6eabeffa06f127f3bfd757f4044668c
Alexander Golubenko <webgolub@gmail.com>
2023-01-03 14:19:55 +0300

    Добавит возможность переинициализации pointPresenter

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,10 +1,11 @@
-import { render, replace } from '../framework/render.js';
+import { remove, render, replace } from '../framework/render.js';
 import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 
 
 export default class PointPresenter {
   #pointsListContainer = null;
+  #point = null;
   #newPoint = null;
   #newEditPoint = null;
 
@@ -12,24 +13,42 @@ export default class PointPresenter {
     this.#pointsListContainer = pointsListContainer;
   }
 
-  #escKeydownHandler = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      this.#handleCloseForm();
-    }
-  };
-
   init(point) {
-    this.#newPoint = new PointView({...point,
+
+    this.#point = point;
+
+    const prevPoint = this.#newPoint;
+    const prevEditPoint = this.#newEditPoint;
+
+    this.#newPoint = new PointView({...this.#point,
       handleExpandButtonClick: this.#handleOpenForm
     });
 
-    this.#newEditPoint = new PointEditView({... point,
+    this.#newEditPoint = new PointEditView({... this.#point,
       handleSubmitForm: this.#handleCloseForm,
       handleRollupButtonClick: this.#handleCloseForm
     });
 
-    render(this.#newPoint, this.#pointsListContainer);
+    if (prevPoint === null || prevEditPoint === null) {
+      render(this.#newPoint, this.#pointsListContainer);
+      return;
+    }
+
+    if (this.#pointsListContainer.contains(prevPoint.element)) {
+      replace(this.#newPoint, prevPoint);
+    }
+
+    if (this.#pointsListContainer.contains(prevEditPoint.element)) {
+      replace(this.#newEditPoint, prevEditPoint);
+    }
+
+    remove(prevPoint);
+    remove(prevEditPoint);
+  }
+
+  destroy() {
+    remove(this.#newPoint);
+    remove(this.#newEditPoint);
   }
 
   #replacePointToForm() {
@@ -48,5 +67,12 @@ export default class PointPresenter {
   #handleOpenForm = () => {
     this.#replacePointToForm();
     document.addEventListener('keydown', this.#escKeydownHandler);
+  };
+
+  #escKeydownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.#handleCloseForm();
+    }
   };
 }
